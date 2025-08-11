3cd830280be8cf86bcd25ab3aaa7658cdd474a67
Damian <46084673+kijoyoo@users.noreply.github.com>
2025-08-11 10:37:49 +0900

    Update Hello.java

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
         System.out.println("no2");
         System.out.println("no3");
         System.out.println("no4");
-
+        System.out.println("no5");
         while (true) {
             Thread.sleep(1000);
         }
