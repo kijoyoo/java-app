2a6fa5a226a1b13f0758f1deabab10561421ef70
kijoyoo <youkijo@daum.net>
2025-08-11 10:20:18 +0900

    java edit

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,8 +1,11 @@
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from GitHub Actions!");
         System.out.println("no2");
         System.out.println("no3");
 
+        while(true) {
+            Thread.sleep(1000);
+        }
     }
-}
+
