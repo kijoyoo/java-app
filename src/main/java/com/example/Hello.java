f170c23611b35dde50bc87cd8431de838abc556f
Damian <46084673+kijoyoo@users.noreply.github.com>
2025-08-12 11:07:42 +0900

    넘버 6

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
         System.out.println("no3");
         System.out.println("no4");
         System.out.println("no5");
-
+        System.out.println("no6");
+        
          while (true) {
             Thread.sleep(1000);
         }
